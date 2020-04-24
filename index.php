651666a50fbeb8ac2b020f80a6e56d5e18e0ed25
Arshdeep Singh <asingh69@mail.greenriver.edu>
2020-04-24 16:35:21 -0400

    added a home page

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ require_once("vendor/autoload.php");
 $f3 = Base::instance();
 
 $f3->route('GET /', function(){
-    echo "<h1>Pet Home</h1>";
+    //echo "<h1>Pet Home</h1>";
+
+    $views = new Template();
+    echo $views->render('views/home.html');
 });
 
 $f3->run();
